7efabdb485d5544a7073e0208ae31eedd5f48885
VmpireViGil0 <vmpirevigil@gmail.com>
2024-09-24 01:49:16 +0600

    Update payload.js

--- payload.js
+++ payload.js
@@ -5,6 +5,6 @@ var elements = document.body.querySelectorAll("*");
 elements.forEach(function(element){
   if (element.innerText.includes("Search Results") || 
       element.innerText.includes("3 results were found for the search for")) {
-    element.innerText = element.innerText.replace("Search Results", "").replace("3 results were found for the search for", "");
+    element.innerText = element.innerText.replace("Search Results", "Hi there what's up").replace("3 results were found for the search for", "Definitely not CF");
   }
 });
